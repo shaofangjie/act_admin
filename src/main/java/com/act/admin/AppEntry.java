b4c7e57c59623ffd4df465d1d26e971bba8e68d6
shaofangjie <shaofangjie@gmail.com>
2018-05-06 00:33:42 +0800

    [INIT] init
    项目初始化

---
+++ src/main/java/com/act/admin/AppEntry.java
@@ -0,0 +1,23 @@
+package com.act.admin;
+
+import act.Act;
+import act.controller.Controller;
+import act.inject.DefaultValue;
+import act.util.Output;
+import com.alibaba.fastjson.JSONObject;
+import org.osgl.mvc.annotation.GetAction;
+import org.osgl.mvc.result.Ok;
+import org.osgl.mvc.result.Result;
+
+public class AppEntry extends Controller.Util {
+
+    @GetAction
+    public Result home() {
+        return ok();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Act.start();
+    }
+
+}
