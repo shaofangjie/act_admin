017bd28bff534846fd1c9ab48b7a9ad80e8ee9e6
shaofangjie <shaofangjie@gmail.com>
2018-05-06 00:14:31 +0800

    Initial commit

---
+++ src/main/java/com/act/admin/act-admin/AppEntry.java
@@ -0,0 +1,19 @@
+package com.act.admin.act-admin;
+
+import act.Act;
+import act.inject.DefaultValue;
+import act.util.Output;
+import org.osgl.mvc.annotation.GetAction;
+
+@SuppressWarnings("unused")
+public class AppEntry {
+
+    @GetAction
+    public void home(@DefaultValue("Hello Act") @Output String msg) {
+    }
+
+    public static void main(String[] args) throws Exception {
+        Act.start();
+    }
+
+}
